7a4855d8cd20505f16368eefc2b6928b09a143f3
Leona Cerimi <cerimileona@gmail.com>
2023-05-01 15:04:51 +0200

    Initial commit

---
+++ src/Temperaturwerte.java
@@ -0,0 +1,16 @@
+public class Temperaturwerte {
+
+    public static void main(String args[]){
+
+        double celsius=10;
+        double Kelvin;
+        double Fahrenheit;
+
+        Fahrenheit= celsius*1.8+32;
+        Kelvin = celsius+ 273.15;
+
+        System.out.println(celsius+"Grad sind" +Fahrenheit+ " Fahrenheit");
+        System.out.println(celsius+"Grad sind" +Kelvin+" Kelvin");
+
+    }
+}
